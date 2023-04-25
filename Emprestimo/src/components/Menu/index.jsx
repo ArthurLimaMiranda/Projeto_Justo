298edcc70bc54f4e7207ab1875369695a04d2dce
Lyssa <lyssa.rodrigues@upe.br>
2023-04-25 20:44:30 -0300

    emprestimo

--- Emprestimo/src/components/Menu/index.jsx
+++ Emprestimo/src/components/Menu/index.jsx
@@ -4,15 +4,15 @@ export function Menu() {
   return (
     <div className="menu">
       <h3>EMPRÉSTIMO</h3>
-      <h3>{/*Guia de Navegação*/}
+      <h3>
           <p className='justa'>Justa</p>
-          <p className='traço'>{/*traço*/}</p>
-          <p className='bloco1'>{/*bloco1*/}</p>
-          <p className='bloco2'>{/*bloco2*/}</p>
-          <p className='bloco3'>{/*bloco3*/}</p>
-          <p className='bloco4'>{/*bloco4*/}</p>
-          <p className='bloco5'>{/*bloco5*/}</p>
-          <p className='bloco6'>{/*bloco6*/}</p>
+          <p className='traço'></p>
+          <p className='bloco1'></p>
+          <p className='bloco2'></p>
+          <p className='bloco3'></p>
+          <p className='bloco4'></p>
+          <p className='bloco5'></p>
+          <p className='bloco6'></p>
           <p className='telainicial'>Tela Inicial</p>
           <p className='saldodevendas'>Saldo de Vendas</p>
           <p className='lançamentos'>Lançamentos</p>
