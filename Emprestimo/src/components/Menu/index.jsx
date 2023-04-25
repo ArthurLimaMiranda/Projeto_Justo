1a07be266ba3e4c42fff83115aa1189445565679
Lyssa <lyssa.rodrigues@upe.br>
2023-04-25 20:19:36 -0300

    emprestimo

--- Emprestimo/src/components/Menu/index.jsx
+++ Emprestimo/src/components/Menu/index.jsx
@@ -4,6 +4,24 @@ export function Menu() {
   return (
     <div className="menu">
       <h3>EMPRÉSTIMO</h3>
+      <h3>{/*Guia de Navegação*/}
+          <p className='justa'>Justa</p>
+          <p className='traço'>{/*traço*/}</p>
+          <p className='bloco1'>{/*bloco1*/}</p>
+          <p className='bloco2'>{/*bloco2*/}</p>
+          <p className='bloco3'>{/*bloco3*/}</p>
+          <p className='bloco4'>{/*bloco4*/}</p>
+          <p className='bloco5'>{/*bloco5*/}</p>
+          <p className='bloco6'>{/*bloco6*/}</p>
+          <p className='telainicial'>Tela Inicial</p>
+          <p className='saldodevendas'>Saldo de Vendas</p>
+          <p className='lançamentos'>Lançamentos</p>
+          <p className='simulaçoes'>Simulações</p>
+          <p className='boleto'>Gerar Boleto</p>
+          <p className='emprestimo'>Empréstimo</p>
+          
+      </h3>
+
     </div>
   );
 }
--- Emprestimo/src/pages/Home.jsx
+++ Emprestimo/src/pages/Home.jsx
@@ -1,10 +1,24 @@
 
+import React, {useState} from "react";
 import './styles.css';
 
 import {Card} from '../components/Card';
 import {Menu} from '../components/Menu';
 
+
 function Home() {
+  const [emprestimoValor, setEmprestimoValor] = useState("");
+  const [emprestimoParcelas, setEmprestimoParcelas] = useState("");
+  const [emprestimo, setEmprestimo]= useState([]);
+
+  function handleAddEmprestimo(){
+     const newEmprestimo = {
+      valor: emprestimoValor,
+      parcelas: emprestimoParcelas
+     };
+  
+  setEmprestimo((prevState)=> [...prevState, newEmprestimo]);
+    }
   return (
     
     <div className="container">
@@ -12,15 +26,16 @@ function Home() {
 <Menu />
   
     <h3>Valor do empréstimo </h3>
-    <input typ="number" placeholder="(R$ 500 - R$ 10.000)" />
+    <input type="number" placeholder="R$ 500 - R$ 10.000"   onChange={(e) => setEmprestimoValor(e.target.value)}  />
       <p></p>
     <h3>Quantidade de parcelas </h3>
-    <input type="number" placeholder="X " />
+    <input type="number" placeholder="X "   onChange={(e) => setEmprestimoParcelas(e.target.value)} />
  
     <p></p>
-      <button type="button">Confirmar</button>
+      <button type="button" onClick={handleAddEmprestimo}>Confirmar</button>
       
-      <Card v/>  
+      { emprestimo.map((emprestimo) => <Card valor= {emprestimo.valor} parcelas= {emprestimo.parcelas}/>  )}
+
       
       </div>
     
