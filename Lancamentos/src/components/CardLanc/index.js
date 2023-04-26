23b6f78acac3e3ee21eaf73fce27cb636e63b263
Arthur Lima <arthurLM@protonmail.com>
2023-04-26 01:29:58 -0300

    Responsividade concluida; detalhes adicionais

--- Lancamentos/src/components/CardLanc/index.js
+++ Lancamentos/src/components/CardLanc/index.js
@@ -5,7 +5,7 @@ export function CardLanc(props){
     return(
         <div className='lanc'>
             <h3 className='tipo'><strong>{props.modoPagamento}</strong></h3>
-            <h6><strong className='Val'>Valor: <span className='valor'>R${props.valor}</span></strong></h6>
+            <h6><strong className='Val'>Valor: <span className={(props.lancamaneto=="Despesas") ? 'desp' : 'receb'}>R${props.valor}</span></strong></h6>
             <span className='layer'>
                 <h6><span className='classe'>Tipo: {props.lancamaneto}</span></h6>   
                 <span className='layer2'>
