d42a2baf33c05560ee2bc413c12835323ef5d314
unknown <diogovas04@gmail.com>
2023-04-26 00:51:53 -0300

    Linkado ao json e novas cores

--- Extrato/src/components/Extrato/Card/index.jsx
+++ Extrato/src/components/Extrato/Card/index.jsx
@@ -3,6 +3,7 @@ import './styles.css'
 function Card(props) {
     return(
         <div className="card">
+            <strong>{props.date}</strong>
             <strong>{props.name}</strong>
             {props.amount < 0 ? <strong className='red'>R$ {props.amount}</strong> : <strong className='green'>R${props.amount}</strong>}
         </div>
