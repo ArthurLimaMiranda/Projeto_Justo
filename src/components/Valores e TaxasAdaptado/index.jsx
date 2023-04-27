9972735061936b6ed3a641b6f37a65e5d80f981c
Arthur Lima <arthurLM@protonmail.com>
2023-04-27 07:45:56 -0300

    Ultimo commit; ajeitando responsividade

--- src/components/Valores e TaxasAdaptado/index.jsx
+++ src/components/Valores e TaxasAdaptado/index.jsx
@@ -3,10 +3,16 @@ export function Valor(props){
     
     return(
         <div className={vtStyle.Coluna2}>
+            <strong className={vtStyle.griditem}>Forma de Pagamento</strong>
+            <strong className={vtStyle.griditem}>Valor a Ser Recebido</strong>
+            <strong className={vtStyle.griditem}>Valor a Ser Cobrado</strong>
+            <strong className={vtStyle.griditem}>Débito</strong>
             <strong className={vtStyle.griditem}>R${props.debito}</strong>
             <strong className={vtStyle.griditem}>R${props.recdebito}</strong>
+            <strong className={vtStyle.griditem}>Crédito à Vista</strong>
             <strong className={vtStyle.griditem}>R${props.creditovista}</strong>
             <strong className={vtStyle.griditem}>R${props.reccreditovista}</strong>
+            <strong className={vtStyle.griditem}>Crédito Parcelado</strong>
             <strong className={vtStyle.griditem}>R${props.creditoparcelado}</strong>
             <strong className={vtStyle.griditem}>R${props.recparcelado}</strong>
         </div>
--- src/pages/SimulacaoAdaptado/Index.jsx
+++ src/pages/SimulacaoAdaptado/Index.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import simulStyle from "./styles.module.css"
 import Sidebar from '../../components/Inicio/Sidebar/Sidebar'
 import { Valor } from "../../components/Valores e TaxasAdaptado/index";
-import ButtonSidebar from '../../components/Inicio/Button/buttonSidebar';
 
 function Simulacao() {
 
@@ -75,53 +74,32 @@ function Simulacao() {
             <Sidebar />
           </div>
 
-          <div>
-            <ButtonSidebar/>
-          </div>
-
           <div className={simulStyle.Simulator}>
 
             <div className={simulStyle.Titulo}>
+              <a href='http://localhost:5173/Inicio' className={simulStyle.retorno}> <h2>{'<'}</h2> </a>
               <h1>Simulador de Vendas</h1>
             </div>
 
           <div className={simulStyle.Conteudo}>
 
             <div className={simulStyle.Linha1}>
-              <div className={simulStyle.valordoproduto}><h1>Valor do Produto R$</h1></div>
+              <div className={simulStyle.valordoproduto}><h3>Valor do Produto R$</h3></div>
               <input type='number' className={simulStyle.rs} onChange={e => setVoriginal(e.target.value)} />
               <input type='number' className={simulStyle.taxa} onChange={e => setJuros(e.target.value)} />
-              <div className={simulStyle.taxadamaquininha}><h1>Taxa da Maquininha %</h1></div>
+              <div className={simulStyle.taxadamaquininha}><h3>Taxa da Maquininha %</h3></div>
             </div>
 
             <div className={simulStyle.Linha2}>
-              <div className={simulStyle.pagamento}><h1>Forma de Pagamento</h1></div>
+              <div className={simulStyle.pagamento}><h3>Forma de Pagamento</h3></div>
               <input type='text' className={simulStyle.pagamentos} onChange={e => setForma(e.target.value)} />
               <input type='number' className={simulStyle.parcela} onChange={e => setParcelas(e.target.value)} />
-              <div className={simulStyle.ndeparcelas}><h1>Nº de Parcelas</h1></div>
+              <div className={simulStyle.ndeparcelas}><h3>Nº de Parcelas</h3></div>
             </div>
 
             <div className={simulStyle.Fim}>
-              <div className={simulStyle.Linha3}>
-                <p className={simulStyle.formadepagamento}>Forma de Pagamento</p>
-                <p className={simulStyle.valoraserrecebido}>Valor a Ser Recebido</p>
-                <p className={simulStyle.valorasercobrado}>Valor a Ser Cobrado</p>
-              </div>
-
-              <div className={simulStyle.Coluna}>
-
-                <div className={simulStyle.Esq}>
-                  <p className={simulStyle.debito}>Débito</p>
-                  <p className={simulStyle.creditoavista}>Crédito à Vista</p>
-                  <p className={simulStyle.creditoparcelado}>Crédito Parcelado</p>
-
-                </div>
-                <Valor debito={valores[0]} creditovista={valores[1]} creditoparcelado={valores[2]} recdebito={valores[3]}
+              <Valor debito={valores[0]} creditovista={valores[1]} creditoparcelado={valores[2]} recdebito={valores[3]}
                 reccreditovista={valores[4]} recparcelado={valores[5]} />
-              </div>
-            
-
-              
             </div>
 
           </div >
